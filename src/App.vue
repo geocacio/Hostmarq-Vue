7051bf18fed63ee52b1e9ce158bad3c9e4ad8aa5
Cácio Dias <cacio@gginformaticaweb.com>
2024-02-01 12:15:41 -0300

    cria o layout da dashboard"

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="wrapper">
+
+    <div class="sidebar show">
+
+      <div class="header-sidebar">
+        <div class="logo"></div>
+        <button class="toggle-menu">
+          <i class="fa-solid fa-user"></i>
+        </button>
+      </div>
+
+      
+    </div>
+    
+    <div class="main-content">
+      
+      <header class="header-top">
+
+      </header>
+      
+      <div class="body-content">
+
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        
+      </div>
+      
+      <footer class="footer">
+        <span class="copy">Copyright © 2024. All right reserved.</span>
+      </footer>
+      </div>
+
+  </div>
+  
+</template>
