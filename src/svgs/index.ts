0a48bf897bcf98cf94378a59aad68c20861a2f49
Cácio Dias <cacio@gginformaticaweb.com>
2024-02-07 11:36:42 -0300

    cria o visual do atirador

--- src/svgs/index.ts
+++ src/svgs/index.ts
@@ -15,6 +15,11 @@ const icons = {
     'chevrons-left' : '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevrons-left"><polyline points="11 17 6 12 11 7"></polyline><polyline points="18 17 13 12 18 7"></polyline></svg>',
     'chevron-right' : '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-right"><polyline points="9 18 15 12 9 6"></polyline></svg>',
     'chevrons-right' : '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevrons-right"><polyline points="13 17 18 12 13 7"></polyline><polyline points="6 17 11 12 6 7"></polyline></svg>',
+    'facebook': '<svg class="custom-svg" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-facebook"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path></svg>',
+    'twitter': '<svg class="custom-svg" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-twitter"><path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path></svg>',
+    'instagram': '<svg class="custom-svg" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-instagram"><rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line></svg>',
+
+
 }
 
 export default icons;
--- src/views/pages/Habituality.vue
+++ src/views/pages/Habituality.vue
@@ -1,6 +1,237 @@
 <template>
+    <div class="row row-gap-15">
+
+        <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+            <div class="card">
+                <div class="person-item">
+                    <img src="https://codervent.com/gum/nikon/demo/vertical/ltr/assets/images/avatars/avatar-1.png"
+                        class="avatar">
+
+                    <div class="person-info mt-5">
+                        <h3 class="name">Pauline I. Bird</h3>
+                        <span class="description">Webdeveloper</span>
+                    </div>
+
+                    <div class="social-networks mt-3 mb-3">
+                        <a href="#" class="social-network bg-facebook">
+                            <IconComponent name="facebook" />
+                        </a>
+                        <a href="#" class="social-network bg-twitter">
+                            <IconComponent name="twitter" />
+                        </a>
+                        <a href="#" class="social-network bg-instagram">
+                            <IconComponent name="instagram" />
+                        </a>
+                    </div>
+
+                    <ButtonComponent buttonClass="dark-blue w-100" text="Ver" />
+                </div>
+            </div>
+        </div>
+
+        <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+            <div class="card">
+                <div class="person-item">
+                    <img src="https://codervent.com/gum/nikon/demo/vertical/ltr/assets/images/avatars/avatar-2.png"
+                        class="avatar">
+
+                    <div class="person-info mt-5">
+                        <h3 class="name">Pauline I. Bird</h3>
+                        <span class="description">Webdeveloper</span>
+                    </div>
+
+                    <div class="social-networks mt-3 mb-3">
+                        <a href="#" class="social-network bg-facebook">
+                            <IconComponent name="facebook" />
+                        </a>
+                        <a href="#" class="social-network bg-twitter">
+                            <IconComponent name="twitter" />
+                        </a>
+                        <a href="#" class="social-network bg-instagram">
+                            <IconComponent name="instagram" />
+                        </a>
+                    </div>
+
+                    <ButtonComponent buttonClass="dark-blue w-100" text="Ver" />
+                </div>
+            </div>
+        </div>
+
+        <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+            <div class="card">
+                <div class="person-item">
+                    <img src="https://codervent.com/gum/nikon/demo/vertical/ltr/assets/images/avatars/avatar-3.png"
+                        class="avatar">
+
+                    <div class="person-info mt-5">
+                        <h3 class="name">Pauline I. Bird</h3>
+                        <span class="description">Webdeveloper</span>
+                    </div>
+
+                    <div class="social-networks mt-3 mb-3">
+                        <a href="#" class="social-network bg-facebook">
+                            <IconComponent name="facebook" />
+                        </a>
+                        <a href="#" class="social-network bg-twitter">
+                            <IconComponent name="twitter" />
+                        </a>
+                        <a href="#" class="social-network bg-instagram">
+                            <IconComponent name="instagram" />
+                        </a>
+                    </div>
+
+                    <ButtonComponent buttonClass="dark-blue w-100" text="Ver" />
+                </div>
+            </div>
+        </div>
+
+        <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+            <div class="card">
+                <div class="person-item">
+                    <img src="https://codervent.com/gum/nikon/demo/vertical/ltr/assets/images/avatars/avatar-4.png"
+                        class="avatar">
+
+                    <div class="person-info mt-5">
+                        <h3 class="name">Pauline I. Bird</h3>
+                        <span class="description">Webdeveloper</span>
+                    </div>
+
+                    <div class="social-networks mt-3 mb-3">
+                        <a href="#" class="social-network bg-facebook">
+                            <IconComponent name="facebook" />
+                        </a>
+                        <a href="#" class="social-network bg-twitter">
+                            <IconComponent name="twitter" />
+                        </a>
+                        <a href="#" class="social-network bg-instagram">
+                            <IconComponent name="instagram" />
+                        </a>
+                    </div>
+
+                    <ButtonComponent buttonClass="dark-blue w-100" text="Ver" />
+                </div>
+            </div>
+        </div>
+
+        <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+            <div class="card">
+                <div class="person-item">
+                    <img src="https://codervent.com/gum/nikon/demo/vertical/ltr/assets/images/avatars/avatar-5.png"
+                        class="avatar">
+
+                    <div class="person-info mt-5">
+                        <h3 class="name">Pauline I. Bird</h3>
+                        <span class="description">Webdeveloper</span>
+                    </div>
+
+                    <div class="social-networks mt-3 mb-3">
+                        <a href="#" class="social-network bg-facebook">
+                            <IconComponent name="facebook" />
+                        </a>
+                        <a href="#" class="social-network bg-twitter">
+                            <IconComponent name="twitter" />
+                        </a>
+                        <a href="#" class="social-network bg-instagram">
+                            <IconComponent name="instagram" />
+                        </a>
+                    </div>
+
+                    <ButtonComponent buttonClass="dark-blue w-100" text="Ver" />
+                </div>
+            </div>
+        </div>
+
+        <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+            <div class="card">
+                <div class="person-item">
+                    <img src="https://codervent.com/gum/nikon/demo/vertical/ltr/assets/images/avatars/avatar-6.png"
+                        class="avatar">
+
+                    <div class="person-info mt-5">
+                        <h3 class="name">Pauline I. Bird</h3>
+                        <span class="description">Webdeveloper</span>
+                    </div>
+
+                    <div class="social-networks mt-3 mb-3">
+                        <a href="#" class="social-network bg-facebook">
+                            <IconComponent name="facebook" />
+                        </a>
+                        <a href="#" class="social-network bg-twitter">
+                            <IconComponent name="twitter" />
+                        </a>
+                        <a href="#" class="social-network bg-instagram">
+                            <IconComponent name="instagram" />
+                        </a>
+                    </div>
+
+                    <ButtonComponent buttonClass="dark-blue w-100" text="Ver" />
+                </div>
+            </div>
+        </div>
+
+        <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+            <div class="card">
+                <div class="person-item">
+                    <img src="https://codervent.com/gum/nikon/demo/vertical/ltr/assets/images/avatars/avatar-7.png"
+                        class="avatar">
+
+                    <div class="person-info mt-5">
+                        <h3 class="name">Pauline I. Bird</h3>
+                        <span class="description">Webdeveloper</span>
+                    </div>
+
+                    <div class="social-networks mt-3 mb-3">
+                        <a href="#" class="social-network bg-facebook">
+                            <IconComponent name="facebook" />
+                        </a>
+                        <a href="#" class="social-network bg-twitter">
+                            <IconComponent name="twitter" />
+                        </a>
+                        <a href="#" class="social-network bg-instagram">
+                            <IconComponent name="instagram" />
+                        </a>
+                    </div>
+
+                    <ButtonComponent buttonClass="dark-blue w-100" text="Ver" />
+                </div>
+            </div>
+        </div>
+
+        <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+            <div class="card">
+                <div class="person-item">
+                    <img src="https://codervent.com/gum/nikon/demo/vertical/ltr/assets/images/avatars/avatar-8.png"
+                        class="avatar">
+
+                    <div class="person-info mt-5">
+                        <h3 class="name">Pauline I. Bird</h3>
+                        <span class="description">Webdeveloper</span>
+                    </div>
+
+                    <div class="social-networks mt-3 mb-3">
+                        <a href="#" class="social-network bg-facebook">
+                            <IconComponent name="facebook" />
+                        </a>
+                        <a href="#" class="social-network bg-twitter">
+                            <IconComponent name="twitter" />
+                        </a>
+                        <a href="#" class="social-network bg-instagram">
+                            <IconComponent name="instagram" />
+                        </a>
+                    </div>
+
+                    <ButtonComponent buttonClass="dark-blue w-100" text="Ver" />
+                </div>
+            </div>
+        </div>
+
+    </div>
+
     <div class="card">
         <div class="row mt-5">
+            <div class="col-3">
+
+            </div>
             <div class="col-12">
                 <TableComponent />
             </div>
@@ -10,5 +241,7 @@
 </template>
 
 <script setup lang="ts">
+import ButtonComponent from '@/components/ButtonComponent.vue';
+import IconComponent from '@/components/IconComponent.vue';
 import TableComponent from '@/components/TableComponent.vue'
 </script>
