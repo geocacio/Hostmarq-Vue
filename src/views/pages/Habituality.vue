a6cec3f0388e3a70a4c20d4d15ec009b5bf053cf
Cácio Dias <cacio@gginformaticaweb.com>
2024-02-12 16:24:04 -0300

    exibe a habitualidade cadastrada

--- src/views/pages/Habituality.vue
+++ src/views/pages/Habituality.vue
@@ -1,7 +1,7 @@
 <template>
     <BreadcrumbComponent title="Habitualidade" />
 
-    <TableComponent class="mt-5" :items="habitualities" :actions="actions" />
+    <TableComponent class="mt-5" :items="habitualitiesData" :actions="actions" />
 
     <!-- <PaginationComponent class="mt-5" :totalPages="2" :currentPage="1" /> -->
 </template>
@@ -56,10 +56,23 @@ const actions: Action[] = [
     },
 ];
 
+const habitualitiesData = ref([]);
+
 onMounted(async () => {
     try {
         await habituallityStore.fetchHabitualities();
         habitualities.value = habituallityStore.getHabitualities;
+        //mapear os dados para escolher quais campos serão exibidos
+        habitualitiesData.value = habitualities.value.map((item) => {
+            return {
+                id: item.id,
+                date_time: item.date_time,
+                location: item.location.name,
+                event: item.event.name,
+                weapon: `Tipo: ${item.weapon.type.name} - Modelo: ${item.weapon.model.name} - Calibre: ${item.weapon.caliber.name} - Sigma: ${item.weapon.number_sigma}`,
+
+            }
+        });
     } catch (e) {
         console.log(e);
     }
