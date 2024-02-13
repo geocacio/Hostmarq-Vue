8c42d6e73b0bb256631c8a188b517d29e0ffbbf1
Cácio Dias <cacio@gginformaticaweb.com>
2024-02-13 13:50:06 -0300

    configura o toast

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,13 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import './assets/sass/styles.scss'
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(Toast);
 app.use(router)
 
 app.mount('#app')
