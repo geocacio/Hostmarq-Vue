1099f880565766e15321e6409b2ea71dd1b38d21
Cácio Dias <cacio@gginformaticaweb.com>
2024-02-15 19:26:31 -0300

    remove consoles

--- src/api/generalAPI.ts
+++ src/api/generalAPI.ts
@@ -4,7 +4,7 @@ import { useToast } from 'vue-toastification'
 const toast = useToast()
 
 export const index = async (url: string) => {
-    console.log('passou aqui', url);
+    
     try{
         const response = await axiosInstance.get(`${url}`);
         return response.data;
@@ -18,8 +18,7 @@ export const store = async (url: string, TypeData: any) => {
         let link = url != '' ? `${url}` : ``;
         const response = await axiosInstance.post(link, TypeData);
         if(response.data.success){
-            console.log('passou aqui');
-            console.log(response.data.success);
+            
             toast.success(response.data.success);
         }
 
