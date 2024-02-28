0c60d9f501a1ee5617d2bc630df81cdc49028879
Jair Barreto <jairbarreto549@gmail.com>
2024-02-28 11:25:12 -0300

    Ajusta rotas api

--- vite.config.ts
+++ vite.config.ts
@@ -16,8 +16,8 @@ export default defineConfig({
   server: {
     proxy: {
       '^/api': {
-        // target: "http://127.0.0.1:8000/api",
-        target: "http://192.168.0.106:8000/api",
+        target: "http://127.0.0.1:8000/api",
+        // target: "http://192.168.0.106:8000/api",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
         // pathRewrite: { '^/api': '/api' },
