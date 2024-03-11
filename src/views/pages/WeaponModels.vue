8ecdaa59e91efdd5e7f36f925ebe9b73624fcfb2
Cácio Dias <cacio@gginformaticaweb.com>
2024-03-11 16:03:12 -0300

    adiciona o slug de forma dinamica

--- src/views/pages/WeaponModels.vue
+++ src/views/pages/WeaponModels.vue
@@ -3,7 +3,8 @@
 
     <div class="dashboard-header flex-horizontal">
         <div class="search-container">
-            <InputComponent type="text" placeholder="Pesquisar" v-model="search" @input="searchSubmit" />
+            <InputComponent type="text" placeholder="Pesquisar" v-model="search" />
+            <!-- <InputComponent type="text" placeholder="Pesquisar" v-model="search" @input="searchSubmit" /> -->
         </div>
         <div class="dashboard-actions">
 
@@ -42,13 +43,21 @@ import LabelComponent from '@/components/form/LabelComponent.vue';
 import ButtonComponent from '@/components/ButtonComponent.vue';
 import TableComponent from '@/components/TableComponent.vue';
 import { useWeaponModelStore } from '@/stores/modules/weaponModel';
+import type { Action } from '@/types/actionType';
+
+// Acessar os dados do usuário conectado
+import { useAuthStore } from '@/stores/modules/auth';
+const authStore = useAuthStore();
+const loggedInuser = authStore.getUser;
+// Acessar o slug do clube do usuário conectado
+const clubSlug = (loggedInuser as { club?: { slug: string } })?.club?.slug ?? '';
 
 const weaponModelStore = useWeaponModelStore();
 const weaponModel = ref([]);
 
 interface dataTable {
-    id: number;
-    name: string;
+    id: number | string;
+    'Nome': string;
 }
 const dataTable = ref<dataTable[]>([]);
 
@@ -82,7 +91,7 @@ const actions: Action[] = [
 ];
 
 const removeWeaponType = async(itemSlug: string) => {
-    await weaponModelStore.deleteWeaponModel("itaberaba-ct", itemSlug)
+    await weaponModelStore.deleteWeaponModel(clubSlug, itemSlug)
     const index = dataTable.value.findIndex((item) => item.id === itemSlug);
     if(index !== -1){
         dataTable.value.splice(index, 1);
@@ -95,10 +104,10 @@ const editCaliber = async(item: object) => {
 
 onMounted(async () => {
     try {
-        await weaponModelStore.fetchWeaponModels('itaberaba-ct');
+        await weaponModelStore.fetchWeaponModels(clubSlug);
         weaponModel.value = weaponModelStore.getWeaponModels;
 
-        dataTable.value = weaponModel.value.map((item) => {
+        dataTable.value = weaponModel.value.map((item: any) => {
             return {
                 id: item.id,
                 'Nome': item.name,
@@ -125,7 +134,7 @@ const submit = async () => {
     if (validateForm()) {
 
         try {
-            const newWeaponModel: any = await weaponModelStore.createWeaponModel("itaberaba-ct", form);
+            const newWeaponModel: any = await weaponModelStore.createWeaponModel(clubSlug, form);
             document.getElementById('closeModal-new-user')?.click();
             const weaponModel = {
                 id: newWeaponModel.id,
@@ -142,20 +151,20 @@ const submit = async () => {
 
 };
 
-const searchSubmit = async (event: any) => {
-    //buscar somente se tiver mais de 3 caracteres, a não ser que seja para apagar a busca
-    if (event.target.value.length < 3 && event.target.value.length > 0) {
-        return;
-    }
+// const searchSubmit = async (event: any) => {
+//     //buscar somente se tiver mais de 3 caracteres, a não ser que seja para apagar a busca
+//     if (event.target.value.length < 3 && event.target.value.length > 0) {
+//         return;
+//     }
 
-    const url = `users?page=${users.value.current_page}&search=${event.target.value}`;
+//     const url = `users?page=${users.value.current_page}&search=${event.target.value}`;
 
-    try {
-        await userStore.fetchUsers(url);
-        users.value = userStore.getUsers;
-    } catch (error) {
-        console.error(error);
-    }
-};
+//     try {
+//         await userStore.fetchUsers(url);
+//         users.value = userStore.getUsers;
+//     } catch (error) {
+//         console.error(error);
+//     }
+// };
 
 </script>
