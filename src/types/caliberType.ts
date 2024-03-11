9cc979c7e34e089d3c2c318bfcd1ad700bc4d326
Cácio Dias <cacio@gginformaticaweb.com>
2024-03-11 15:55:48 -0300

    adiciona o slug so club de forma dinamica

---
+++ src/types/caliberType.ts
@@ -0,0 +1,9 @@
+export interface Caliber{
+    club_id: number;
+    created_at?: string;
+    id?: number;
+    name: string;
+    slug?: string;
+    type?: string;
+    updated_at?: string;
+}
--- src/views/pages/Calibers.vue
+++ src/views/pages/Calibers.vue
@@ -3,7 +3,8 @@
 
     <div class="dashboard-header flex-horizontal">
         <div class="search-container">
-            <InputComponent type="text" placeholder="Pesquisar" v-model="search" @input="searchSubmit" />
+            <InputComponent type="text" placeholder="Pesquisar" v-model="search" />
+            <!-- <InputComponent type="text" placeholder="Pesquisar" v-model="search" @input="searchSubmit" /> -->
         </div>
         <div class="dashboard-actions">
 
@@ -53,20 +54,30 @@ import BreadcrumbComponent from '@/components/BreadcrumbComponent.vue';
 import PaginationComponent from '@/components/PaginationComponent.vue';
 import InputComponent from '@/components/form/InputComponent.vue';
 import { ref, onMounted, reactive } from 'vue';
-import type { User } from '@/types/userType';
+
 import ModalComponent from '@/components/ModalComponent.vue';
 import LabelComponent from '@/components/form/LabelComponent.vue';
 import ButtonComponent from '@/components/ButtonComponent.vue';
 import TableComponent from '@/components/TableComponent.vue';
 import { useCaliberStore } from '@/stores/modules/caliber';
+import type { Caliber } from '@/types/caliberType';
+import type { Action } from '@/types/actionType';
+
+// Acessar os dados do usuário conectado
+import { useAuthStore } from '@/stores/modules/auth';
+const authStore = useAuthStore();
+const loggedInuser = authStore.getUser;
+// Acessar o slug do clube do usuário conectado
+const clubSlug = (loggedInuser as { club?: { slug: string } })?.club?.slug ?? '';
 
 const caliberStore = useCaliberStore();
 const calibers = ref<Caliber[]>([]);
 
 interface dataTable {
-    id: number;
-    name: string;
-    type: string;
+    id: number | string;
+    'Nome': string;
+    'Tipo': string;
+    slug: string;
 }
 const dataTable = ref<dataTable[]>([]);
 
@@ -96,7 +107,7 @@ const search = ref('');
 const actions: Action[] = [
     {
         name: 'edit',
-        action: (item) => {
+        action: (item: any) => {
             
         },
         icon: 'edit',
@@ -129,8 +140,8 @@ const actions: Action[] = [
 ];
 
 const removeCaliber = async(itemSlug: string) => {
-    await caliberStore.deleteCaliber("itaberaba-ct", itemSlug)
-    const index = dataTable.value.findIndex((item) => item.slug === itemSlug);
+    await caliberStore.deleteCaliber(clubSlug, itemSlug)
+    const index = dataTable.value.findIndex((item: any) => item.slug === itemSlug);
     if (index !== -1) {
         dataTable.value.splice(index, 1);
     }
@@ -142,7 +153,7 @@ const editCaliber = async(item: object) => {
 
 onMounted(async () => {
     try {
-        await caliberStore.fetchCalibers('itaberaba-ct');
+        await caliberStore.fetchCalibers(clubSlug);
         calibers.value = caliberStore.getCalibers;
 
         // await userStore.fetchUsers('users');
@@ -150,10 +161,10 @@ onMounted(async () => {
         // console.log('passou aqui', users.value.data)
         dataTable.value = calibers.value.map((item) => {
             return {
-                id: item.id,
-                'Nome': item.name,
-                'Tipo': item.type,
-                slug: item.slug
+                id: item.id ?? '',
+                'Nome': item.name ?? '',
+                'Tipo': item.type ?? '',
+                slug: item.slug ?? '',
             }
         })
     } catch (error) {
@@ -177,7 +188,7 @@ const submit = async () => {
 
     if (validateForm()) {
         try {
-            const newCaliber: any = await caliberStore.createCaliber("itaberaba-ct", form);
+            const newCaliber: any = await caliberStore.createCaliber(clubSlug, form);
             document.getElementById('closeModal-new-user')?.click();
             let caliber = {
                 id: newCaliber.id,
@@ -195,21 +206,21 @@ const submit = async () => {
 
 };
 
-const searchSubmit = async (event: any) => {
-    //buscar somente se tiver mais de 3 caracteres, a não ser que seja para apagar a busca
-    if (event.target.value.length < 3 && event.target.value.length > 0) {
-        return;
-    }
+// const searchSubmit = async (event: any) => {
+//     //buscar somente se tiver mais de 3 caracteres, a não ser que seja para apagar a busca
+//     if (event.target.value.length < 3 && event.target.value.length > 0) {
+//         return;
+//     }
 
-    const url = `users?page=${users.value.current_page}&search=${event.target.value}`;
+//     const url = `users?page=${users.value.current_page}&search=${event.target.value}`;
 
-    try {
-        await userStore.fetchUsers(url);
-        users.value = userStore.getUsers;
-    } catch (error) {
-        console.error(error);
-    }
-};
+//     try {
+//         await userStore.fetchUsers(url);
+//         users.value = userStore.getUsers;
+//     } catch (error) {
+//         console.error(error);
+//     }
+// };
 
 // const updateuser = async (user: User) => {
 //     try {
